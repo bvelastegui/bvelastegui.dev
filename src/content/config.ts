2626f0d12b98874889310608f845e16f5fb84e62
Bryan Velastegui <bryanslvr@gmail.com>
2025-06-18 15:32:29 -0500

    Integrate `Experience` section with dynamic data loading from JSON and add it to the homepage. Update styles and components for improved consistency and responsiveness.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,5 @@
 import { defineCollection, z } from 'astro:content';
+import { file } from "astro/loaders";
 
 // Define the schema for the projects collection
 const projectsCollection = defineCollection({
@@ -15,7 +16,19 @@ const projectsCollection = defineCollection({
   }),
 });
 
+const experiencesCollection = defineCollection({
+  loader: file('src/data/experiences.json'),
+  schema: z.object({
+    startDate: z.string(),
+    endDate: z.string().optional(),
+    company: z.string(),
+    position: z.string(),
+    description: z.string(),
+  }),
+});
+
 // Export the collections
 export const collections = {
   'projects': projectsCollection,
+  'experiences': experiencesCollection,
 };
