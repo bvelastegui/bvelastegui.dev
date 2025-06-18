9d0887cc82c2bc7604f8ee96e23aed3bd45533e7
Bryan Velastegui <bryanslvr@gmail.com>
2025-06-18 18:45:47 -0500

    Add Node.js adapter and passthrough image service to Astro configuration. Update dependencies accordingly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  image: {
+    service: passthroughImageService(),
+  },
+  adapter: node({
+    mode: "standalone",
+  }),
 });
