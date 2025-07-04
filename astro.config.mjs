e427e1b5d3f85dbda511cdbc4db4801786d0b391
Bryan Velastegui <bryanslvr@gmail.com>
2025-07-04 13:31:36 -0500

    Integrate Partytown for improved script offloading. Update Astro configuration, add Google Tag Manager setup, and adjust dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,28 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import node from "@astrojs/node";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   image: {
     service: passthroughImageService(),
   },
+
   adapter: node({
     mode: "standalone",
   }),
+
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
